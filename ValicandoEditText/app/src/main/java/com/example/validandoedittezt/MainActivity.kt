30f2878147e0725a64b51683786f8050c1803e81
Jessica Fernandes <jessicafernandes.055@gmail.com>
2021-01-27 20:52:09 -0300

    atualizacao exercicios

--- ValicandoEditText/app/src/main/java/com/example/validandoedittezt/MainActivity.kt
+++ ValicandoEditText/app/src/main/java/com/example/validandoedittezt/MainActivity.kt
@@ -6,12 +6,15 @@ import android.widget.Toast
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
 
     private val floatButton by lazy { findViewById<FloatingActionButton>(R.id.btn_check) }
     private val editName by lazy { findViewById<TextInputEditText>(R.id.edit_nome) }
     private val editEmail by lazy { findViewById<TextInputEditText>(R.id.edit_email) }
+    private val layoutName by lazy { findViewById<TextInputLayout>(R.id.text_layout_nome) }
+    private val layoutEmail by lazy { findViewById<TextInputLayout>(R.id.text_layout_email) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,10 +31,21 @@ class MainActivity : AppCompatActivity() {
     fun validateLogin(nome: String, email: String) {
     //"Validar nome e email estão corretos, se forem corretos subir um SnackBar dizendo 'Sucesso'")
         when {
-            nome.isBlank() && email.isBlank() -> {
-                Snackbar.make(floatButton, "Erro", Snackbar.LENGTH_LONG).show()
+            nome.isBlank() && email.isBlank()-> {
+                layoutName?.error = "Required"
+                layoutEmail?.error = "Required"
+            }
+            nome.isBlank() -> {
+                layoutName?.error = "Required"
+                layoutEmail?.error = null
+            }
+            email.isBlank()-> {
+                layoutName?.error = null
+                layoutEmail?.error = "Required"
             }
             else -> {
+                layoutName?.error = null
+                layoutEmail?.error = null
                 Snackbar.make(floatButton, "Sucesso", Snackbar.LENGTH_LONG).show()
             }
         }
