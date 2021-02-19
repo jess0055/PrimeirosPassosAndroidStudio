0394b888539b1d98c2bdc044962d2a090aba24bf
Jessica Fernandes <jessicafernandes.055@gmail.com>
2021-02-18 21:17:21 -0300

    organizando aulas

--- Agenda/app/src/main/java/com/example/agenda/view/MainActivity.kt
+++ Agenda/app/src/main/java/com/example/agenda/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.agenda
+package com.example.agenda.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.agenda.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
